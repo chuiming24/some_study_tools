d7726f663c126bb7682f28e074d442e3c5a29d9c
二四 <794960040@qq.com>
2019-01-23 09:45:08 +0800

    added test code

--- cleanexif/src/cleanexif.py
+++ cleanexif/src/cleanexif.py
@@ -3,8 +3,8 @@ from glob import glob as find
 from os import path
 
 if __name__ == '__main__':
-    # a = piexif.load(fileName)
-    PATH = r'C:\ddz_x86\text'  # 存放图片的文件夹路径
+    # exif_info = piexif.load(fileName)
+    # print(exif_info)
     PATH = input(r"请输入存放图片的文件夹路径，比如：C:\text" + "\n")
     SUFF = input(r"请输入要批量去除EXIF的图片后缀名，比如：jpg" + "\n")
     paths = find(path.join(PATH, '*.'+SUFF))
